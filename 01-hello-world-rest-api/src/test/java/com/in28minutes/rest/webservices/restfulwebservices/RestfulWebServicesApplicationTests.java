f30ca9033eeb2485bfde6b197e3fbe0598adba9e
helen <hhma@alumni.cpp.edu>
2020-04-12 17:49:34 -0400

    add test case

--- 01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ 01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,16 +1,25 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
 public class RestfulWebServicesApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response =
+				testRestTemplate.getForEntity("http://hello-world-rest-api-hhma-100.cfapps.io/hello-world/path-variable/good_morning", String.class);
+        String expected = "{\"message\":\"Hello World, good_morning\"}";
+		assertTrue(response.getBody().contains(expected));
 	}
 
 }
