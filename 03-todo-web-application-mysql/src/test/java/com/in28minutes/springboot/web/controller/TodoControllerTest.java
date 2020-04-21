4e4604bcb7e5d2c1453c5c69bc357bdaf983c600
helen <hhma@alumni.cpp.edu>
2020-04-21 18:28:14 -0400

    modify the request uri.

--- 03-todo-web-application-mysql/src/test/java/com/in28minutes/springboot/web/controller/TodoControllerTest.java
+++ 03-todo-web-application-mysql/src/test/java/com/in28minutes/springboot/web/controller/TodoControllerTest.java
@@ -1,19 +1,13 @@
 package com.in28minutes.springboot.web.controller;
 
-import com.in28minutes.springboot.web.model.Todo;
-import com.in28minutes.springboot.web.service.TodoRepository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
@@ -30,10 +24,8 @@ class TodoControllerTest {
     @Test
     @WithMockUser(username="in30minutes", password="dummy", roles="USER")
     public void showTodos() throws Exception {
-      TodoRepository repository = todoController.getRepository();
-      List<Todo> todos = repository.findByUser("in30minutes");
-      mockMvc.perform(get("/list-todos/"))
-               .andExpect(model().attribute("todos", todos ))
-              .andExpect(view().name("list-todos"));
-    }
+
+     mockMvc.perform(get("/list-todos/"))
+            .andExpect(view().name("list-todos"));
+     }
 }
