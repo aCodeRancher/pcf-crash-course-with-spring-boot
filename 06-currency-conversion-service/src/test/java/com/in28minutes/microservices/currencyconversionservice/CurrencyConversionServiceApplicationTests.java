3c62fe24725e122e64484036029e4c62528497e9
helen <hhma@alumni.cpp.edu>
2020-05-05 14:34:38 -0400

    junit test for integration without the cloud config server.

--- 06-currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
+++ 06-currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -13,8 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 public class CurrencyConversionServiceApplicationTests {
 
-	//Before running this integration test , run the Eureka naming service, currency exchange service and
+	//Before running this integration test , run eureka naming server, spring cloud config server, currency exchange service and
 	//currency conversion service.
+    //When cloud config server is up, the conversion profit percent = 60 by default
+    //conversionMultiple = (100-60)/100 = 0.4
+    //convertedValue = 100*65*0.4=2600
 	@Test
 	public void contextLoads() throws RestClientException {
       TestRestTemplate testRestTemplate = new TestRestTemplate();
@@ -26,11 +29,31 @@ public class CurrencyConversionServiceApplicationTests {
       assertTrue(responseBean.getFrom().equals("USD"));
       assertTrue(responseBean.getTo().equals("INR"));
       assertTrue(responseBean.getConversionMultiple().intValue() == 65);
-      assertTrue(responseBean.getTotalCalculatedAmount().intValue() == 6500 );
+      assertTrue(responseBean.getTotalCalculatedAmount().intValue() == 2600 );
       assertTrue(responseBean.getId() == 10001);
    }
 
+    //Before running this test, don't run the cloud config server. Just run
+    //Eureka, exchange service and conversion service.
+    //When cloud config server is not up, the conversion profit percent = 5 by default
+    //conversionMultiple = (100-5)/100 = 0.95
+    //convertedValue = 100*65*0.95=6175
     @Test
+    public void contextLoads_withoutConfigServer() throws RestClientException {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        ResponseEntity<CurrencyConversionBean> response = testRestTemplate.exchange(
+                "http://localhost:8100/currency-converter/from/{from}/to/{to}/quantity/{quantity}",
+                HttpMethod.GET, null, CurrencyConversionBean.class, "USD", "INR", "100");
+        CurrencyConversionBean responseBean = response.getBody();
+        assertTrue(responseBean.getQuantity().equals(BigDecimal.valueOf(100)));
+        assertTrue(responseBean.getFrom().equals("USD"));
+        assertTrue(responseBean.getTo().equals("INR"));
+        assertTrue(responseBean.getConversionMultiple().intValue() == 65);
+        assertTrue(responseBean.getTotalCalculatedAmount().intValue() == 6175 );
+        assertTrue(responseBean.getId() == 10001);
+    }
+
+   /* @Test
     public void contextLoads_usingServiceRegistry() throws RestClientException {
 	    //This is just my route. Other people will have different routes.
 	    String route = "currency-conversion-service-hma-101.cfapps.io";
@@ -45,5 +68,5 @@ public class CurrencyConversionServiceApplicationTests {
         assertTrue(responseBean.getConversionMultiple().intValue() == 65);
         assertTrue(responseBean.getTotalCalculatedAmount().intValue() == 6500 );
         assertTrue(responseBean.getId() == 10001);
-    }
+    }*/
 }
