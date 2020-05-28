0a1d4b91c02ea41f816d21d6fe0f1eb87891e439
helen <hhma@alumni.cpp.edu>
2020-05-27 20:22:26 -0400

    remove @ComponentScan

--- 02-todo-web-application-h2/src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
+++ 02-todo-web-application-h2/src/main/java/com/in28minutes/springboot/web/SpringBootFirstWebApplication.java
@@ -7,13 +7,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.in28minutes.springboot.web")
-public class SpringBootFirstWebApplication extends SpringBootServletInitializer {
+//@ComponentScan("com.in28minutes.springboot.web")
+public class SpringBootFirstWebApplication  extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootFirstWebApplication.class);
-	}
+	 @Override
+ 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	 	return application.sources(SpringBootFirstWebApplication.class);
+ }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstWebApplication.class, args);
