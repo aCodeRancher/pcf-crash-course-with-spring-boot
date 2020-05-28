5ff52f7626d06396c56786d62eafb0a3dc36b11a
helen <hhma@alumni.cpp.edu>
2020-05-27 20:21:42 -0400

    add test after deployed to PCF

--- 01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ 01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,12 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+		import org.junit.jupiter.api.Test;
+		import org.springframework.boot.test.context.SpringBootTest;
+		import org.springframework.boot.test.web.client.TestRestTemplate;
+		import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+		import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -18,7 +18,7 @@ public class RestfulWebServicesApplicationTests {
 		TestRestTemplate testRestTemplate = new TestRestTemplate();
 		ResponseEntity<String> response =
 				testRestTemplate.getForEntity("http://hello-world-rest-api-hhma-100.cfapps.io/hello-world/path-variable/good_morning", String.class);
-        String expected = "{\"message\":\"Hello World, good_morning\"}";
+		String expected = "{\"message\":\"Hello World, good_morning\"}";
 		assertTrue(response.getBody().contains(expected));
 	}
 
